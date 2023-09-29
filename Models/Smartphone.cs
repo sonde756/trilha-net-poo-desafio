ac3597b86738cc17349d5eb2330daec5c9717de3
sonde756 <edsonrafael756@gmail.com>
2023-09-29 00:31:04 -0300

    initial commit

---
+++ Models/Smartphone.cs
@@ -0,0 +1,37 @@
+namespace DesafioPOO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+
+        public string Modelo
+        {
+            get => _modelo;
+
+        }
+
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            _modelo = modelo;
+            _imei = imei;
+            _memoria = memoria;
+        }
+       
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+
+nokia.InstalarAplicativo("Snake");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+
+Iphone iphone = new Iphone("987654321", "iPhone 11", "987654321", 64);
+
+iphone.InstalarAplicativo("Instagram");
+iphone.Ligar();
+iphone.ReceberLigacao();
